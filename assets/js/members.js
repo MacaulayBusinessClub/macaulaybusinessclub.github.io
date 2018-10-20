0c776fa9c7205ed8b7134b480e79b503ee8e873a
Boubacar Diallo <boubacar.dialo1@macaulay.cuny.edu>
2018-10-20 19:37:39 -0400

    leftover pc

--- assets/js/members.js
+++ assets/js/members.js
@@ -13,29 +13,37 @@ function loadmembers(json){
     var members = json["Roster"];
     for(var i = 0; i < members.length;i++){
         var card = "";
-        if(i % 4 == 0){ 
-         $("#memberdisplay").append("<div class='row portrait-row'>");
-        }
+        // if(i % 4 == 0){ 
+        //  $("#memberdisplay").append("<div class='row portrait-row'>");
+        // }
         var member = members[i];
         var name = member["name"];
         var image = member.imageurl;
         var linkedin = member.linkedin;
         var position = member.position;
+        var positiontext = "<p class='card-text'>" + position + "</p>";
 
-        card = "<div class='col-md-3 col-xs-6'>" + 
-                        "<div class = 'member'>" +
-                        "<div class='portrait'>" + 
-                            "<img class='card-img-top member-image' src='" + image + "' alt='" + name + "' width='100%' height='100%'>" + 
+        if(position.length >= 27){
+            positiontext = "<p class='card-text long-position'>" + position + "</p>";
+        }
+        
+        card = "<div class='col-md-3 col-sm-4 col-xs-6 member'>" + 
+                    "<div class='portrait'>" + 
+                        "<div class = 'card'>" +
+                            "<img class='card-img-top member-image' src='images/members/" + image + "' alt='" + name + "' width='100%' height='100%'>" + 
                            "<div class='card-block'>" + 
                                 "<h4 class='card-title'>" + name + "</h4>" +
-                                "<p class='card-text'>" + position + "</p>" + 
+                                 positiontext + 
                                 "<a class='card-social' href='" + linkedin + "'><img src='assets/icons/LinkedIn_48x48.png' width='36' height='36'></a>" + 
-                    " </div> </div> </div> </div>"
+                           "</div>" + 
+                        "</div>" +
+                    "</div>" + 
+                "</div>";
 
           $("#memberdisplay").append(card);
-        if(i % 4 == 3){    
-        $("#memberdisplay").append("</div");
-        }
+        // if(i % 4 == 3){    
+        // $("#memberdisplay").append("</div");
+        // }
     }
 }
     
