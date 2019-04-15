6d997a8a6e4b935266aef5d85000be2741ee5591
Boubacar Diallo <boubacar.dialo1@macaulay.cuny.edu>
2019-04-15 04:14:34 -0400

    articles connected to firebase

--- firebase-app/public/assets/js/loadarticles.js
+++ firebase-app/public/assets/js/loadarticles.js
@@ -1,8 +1,22 @@
+let params = new URLSearchParams(window.location.search); 
+let articleID = params.get('id')
 
-console.log(window.location.search)
-let params = new URLSearchParams(window.location.search);
-
-if(!params.has('id')){
+if(!articleID){
     window.location.replace('../')
 }
 
+
+let database = firebase.database().ref("articles");
+
+
+database.once("value").then((snapshot) =>{
+    let data = snapshot.val();
+    if(!data.posts[articleID]){
+        console.log('error out')
+    }
+    else{
+        console.log(data.posts[articleID].content);
+        console.log($('#article-title'))
+        $('#article-title').text(data.posts[articleID].content);
+    }
+});
